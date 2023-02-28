412f5edbbf043a825777087b0f3fd0b5fc056b81
Jona Goldman <jonagoldman@gmail.com>
2023-02-27 21:57:00 -0300

    support laravel 10

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -6,8 +6,6 @@ use AgilePixels\ResourceAbilities\Tests\Fakes\TestModel;
 
 class CollectionTest extends TestCase
 {
-    use AddsAbilitiesTest;
-
     private TestModel $testModel;
 
     protected function setUp(): void
@@ -18,5 +16,47 @@ class CollectionTest extends TestCase
             'id' => 1,
             'name' => 'testModel',
         ]);
+    }
+
+    /** @test */
+    public function it_returns_abilities()
+    {
+        $this->assertEmpty($this->testModel->getAbilities());
+    }
+
+    /** @test */
+    public function it_adds_abilities()
+    {
+        $this->testModel->checkAbility('ability');
+
+        $this->assertContains('ability', $this->testModel->getAbilities());
+    }
+
+    /** @test */
+    public function it_merges_abilities_to_new_instances()
+    {
+        $this->testModel->checkAbility('ability');
+
+        $newInstance = $this->testModel->newInstance();
+
+        $this->assertContains('ability', $newInstance->getAbilities());
+    }
+
+    /** @test */
+    public function it_adds_an_array_of_abilities()
+    {
+        $this->testModel->checkAbility(['ability', 'another_ability']);
+
+        $this->assertContains('ability', $this->testModel->getAbilities());
+        $this->assertContains('another_ability', $this->testModel->getAbilities());
+    }
+
+    /** @test */
+    public function it_adds_spreaded_abilities()
+    {
+        $this->testModel->checkAbility('ability', 'another_ability');
+
+        $this->assertContains('ability', $this->testModel->getAbilities());
+        $this->assertContains('another_ability', $this->testModel->getAbilities());
     }
 }
